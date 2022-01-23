7e2cb5bfc8502c24eb6363fd6ade0ebde27900ea
Bohdan Stasiv <badyaxa@gmail.com>
2022-01-23 23:24:01 +0200

    Spring_1_Intro_&_Core-6 add Dog, Cat, Cheetah, Spider, Pet

--- src/main/java/com/epam/spring/homework1/config/PetConfig.java
+++ src/main/java/com/epam/spring/homework1/config/PetConfig.java
@@ -1,7 +1,10 @@
 package com.epam.spring.homework1.config;
 
-import org.springframework.context.annotation.Configuration;
+import com.epam.spring.homework1.pet.Spider;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan(basePackages = "com.epam.spring.homework1.pet",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Spider.class))
 public class PetConfig {
 }
