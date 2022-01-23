9f7a327cf5210f8e143413c295dbce951a31738e
Bohdan Stasiv <badyaxa@gmail.com>
2022-01-24 01:18:20 +0200

    Update ApplicationHW_1.java

--- src/main/java/com/epam/spring/homework1/ApplicationHW_1.java
+++ src/main/java/com/epam/spring/homework1/ApplicationHW_1.java
@@ -9,9 +9,5 @@ public class ApplicationHW_1 {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
         context.getBean(Pet.class).printPets();
-        //з контексу бін Cheetah за типом,
-        // за іменем біна і проаналізуйте результат
-//        System.out.println("context.getBean(Cheetah.class).getClass() = " + context.getBean(Cheetah.class).getClass());
-//        context.getBean()
     }
 }
