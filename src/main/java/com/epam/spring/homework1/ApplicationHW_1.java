27077b9853f79762d617a0a63e2c09e0daa258ad
Bohdan Stasiv <badyaxa@gmail.com>
2022-01-24 01:00:09 +0200

    rename main file

--- src/main/java/com/epam/spring/homework1/ApplicationHW_1.java
+++ src/main/java/com/epam/spring/homework1/ApplicationHW_1.java
@@ -5,9 +5,13 @@ import com.epam.spring.homework1.pet.Pet;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Application {
+public class ApplicationHW_1 {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
         context.getBean(Pet.class).printPets();
+        //з контексу бін Cheetah за типом,
+        // за іменем біна і проаналізуйте результат
+//        System.out.println("context.getBean(Cheetah.class).getClass() = " + context.getBean(Cheetah.class).getClass());
+//        context.getBean()
     }
 }
